930e75c9825c3c3e5231f671b35f8c2951047bd3
Eugene <evgen29031999@bk.ru>
2021-12-02 12:36:05 +0300

    First Commit

---
+++ mycustomclass.h
@@ -0,0 +1,33 @@
+#ifndef MYCUSTOMCLASS_H
+#define MYCUSTOMCLASS_H
+
+#include <QList>
+#include <QMetaObject>
+
+
+class MyCustomClass
+{
+public:
+    MyCustomClass();
+
+    template <typename T>
+    static bool Match (const T& object, QList<QString> propNames, QString input)
+    {
+
+        for(const QString& str: propNames){
+
+            for(int i = object->metaObject()->propertyOffset(); i < object->metaObject()->propertyCount(); ++i)
+            {
+
+                if (object.metaObject().property(i).name() == str && object.metaObject().property(i).read(object).toString().startsWith(input)){
+
+
+
+                }
+            }
+        }
+        return false;
+    }
+};
+
+#endif // MYCUSTOMCLASS_H
