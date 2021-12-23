9db7287c316c8ec0e37934be566a5777e6c8110f
Eugene <evgen29031999@bk.ru>
2021-12-23 10:07:06 +0300

    complete Deserealizer

--- deserealizator.h
+++ deserealizator.h
@@ -1,11 +1,40 @@
 #ifndef DESEREALIZATOR_H
 #define DESEREALIZATOR_H
 
+#include <QList>
+#include <QObject>
+#include <QJsonValue>
+#include <QStringList>
+#include <QJsonObject>
+#include <QMetaObject>
+#include <QMetaProperty>
+
+#include <iostream>
 
 class Deserealizator
 {
 public:
     Deserealizator();
+
+private:
+
+template<typename T>
+    static T* Deserealize(QJsonValue jv)
+    {   
+        T* t = new T();
+        int amountOfProp = t->metaObject()->propertyOffset();
+        QStringList propNames = jv.toObject().keys();
+
+        for(int i = 0; i = amountOfProp; ++i){
+            for(const QString& name: propNames){
+                if(t->metaObject()->property(i).name() == name.toStdString()){
+                    t->metaObject()->property(i).write(&t, name);
+                }
+            }
+        }
+
+        return t;
+    }
 };
 
 #endif // DESEREALIZATOR_H
